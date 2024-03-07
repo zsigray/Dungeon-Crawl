ad19a8843b57df7f90d2184c4ad60445de3c7dc5
Sara Soos <142304198+soosdsara@users.noreply.github.com>
2024-03-07 13:40:01 +0100

    Refactor and placement of characters

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -3,7 +3,6 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.Drawable;
-import javafx.scene.canvas.GraphicsContext;
 
 import java.util.Objects;
 
--- src/main/java/com/codecool/dungeoncrawl/ui/UI.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/UI.java
@@ -70,11 +70,11 @@ public class UI {
                             cell.setType(CellType.DOOROPEN);
                         }
                     }
-                    Tiles.drawTile(context, cell.getActor(), x, y);
+                    Tiles.drawTile(context, cell.getActor(), x, y, 1.5);
                 } else if (cell.getItem() != null) {
-                    Tiles.drawTile(context, cell.getItem(), x, y);
+                    Tiles.drawTile(context, cell.getItem(), x, y, 1.5);
                 } else {
-                    Tiles.drawTile(context, cell, x, y);
+                    Tiles.drawTile(context, cell, x, y, 1.5);
                 }
             }
         }
